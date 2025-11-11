41b3ba100f68eeadd03fb3c0f2e09e6a23794196
juan david <juandavidcastrillonzapata8@gmail.com>
2025-11-11 15:30:27 -0500

    Agregados componente-1 y servicio-1

---
+++ src/app/componente-1/componente-1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-1',
+  imports: [],
+  templateUrl: './componente-1.html',
+  styleUrl: './componente-1.css',
+})
+export class Componente1 {
+
+}
